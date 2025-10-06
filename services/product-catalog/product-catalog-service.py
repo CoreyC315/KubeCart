80968034814d0d6e671fda338c60d35e7d665c28
Corey Campbell <ccampbell@rosemedical.com>
2025-10-06 09:27:51 -0400

    testing the ci/cd 5

--- services/product-catalog/product-catalog-service.py
+++ services/product-catalog/product-catalog-service.py
@@ -23,7 +23,7 @@ PRODUCTS = {
         "price": 199.99,
         "description": "High-performance 6-core processor.",
         "socket": "AM4",
-        "version": "CI/CD Test 1.0"
+        "version": "CI/CD Test 1.1"
     },
     "P002": {
         "id": "P002",
